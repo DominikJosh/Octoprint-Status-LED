ed7a92969ea4bbfb187ce1f9dff2c27c67c07c06
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2018-11-13 05:54:42 +0100

    Update config.h

--- src2/config.h
+++ src2/config.h
@@ -11,7 +11,7 @@
   const char* APIKEY = ""; // octoprint api key (optain it in the web interface)
   String OCTOIP = ""; // the ip address of octoprint
   uint16_t pollInterval = 500; // ms, lower value will increase poll rate, but the animation time will limit the poll rate too
-  const uint8_t length = 7; // number of LEDs
+  const uint8_t length = 8; // number of LEDs, minimum amount of LEDs is 4 better ist 8 and the aquivalent of it (8,18,24,32,40....)
   int statusShow = true;   //set false for white light while printing and while printer is ready
 
 #endif
