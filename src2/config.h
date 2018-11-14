4cef2a46ca30f44f31f2fa78578cbba6541f0354
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2018-11-14 19:12:11 +0100

    Update config.h

--- src2/config.h
+++ src2/config.h
@@ -12,6 +12,7 @@
   String OCTOIP = ""; // the ip address of octoprint
   uint16_t pollInterval = 500; // ms, lower value will increase poll rate, but the animation time will limit the poll rate too
   const uint8_t length = 8; // number of LEDs, minimum amount of LEDs is 4 better ist 8 supported: 4,8,12,16,20,24,28,32,36,40...120)
-  int statusShow = true;   //set false for white light while printing and while printer is ready
+  int statusShow = true;   //set false for white light while printing 
+  int readyShow = true;    //set false for white light while printer is ready
 
 #endif
