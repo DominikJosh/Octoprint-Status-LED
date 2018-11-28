18ef75cf9c9f4e586b7e21fa5c8b632676553c44
DominikJosh <42120789+DominikJosh@users.noreply.github.com>
2018-11-28 18:40:01 +0100

    Update config.h

--- src2/config.h
+++ src2/config.h
@@ -4,10 +4,10 @@
 #ifndef CONFIG  // not #ifnotdef
 #define CONFIG
 #define PIN D2
-  const char* SSID = "FRITZ!Box 6360 Cable"; // wlan SSID
-  const char* WPWD = "9884406786530457"; // wlan password
-  const char* APIKEY = "C0A147736CF443AA8078CEEC4C1BE53B"; // octoprint api key (optain it in the web interface)
-  String OCTOIP = "192.168.178.48"; // the ip address of octoprint
+  const char* SSID = "Put SSID Here"; // wlan SSID
+  const char* WPWD = "Put WLAN Password Here"; // wlan password
+  const char* APIKEY = "put api key here"; // octoprint api key (optain it in the web interface)
+  String OCTOIP = "192.168.178.0"; // the ip address of octoprint
   uint16_t pollInterval = 500; // ms, lower value will increase poll rate, but the animation time will limit the poll rate too
   const uint8_t length = 8; // number of LEDs, minimum amount of LEDs is 4 better ist 8 supported: 4,8,12,16,20,24,28,32,36,40...120)
   int statusShow = false;   //set false for white light while printing 
